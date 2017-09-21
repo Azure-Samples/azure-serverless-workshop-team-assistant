dc27d82295103c74c420bbcffcadb0349f0d9822
Kanio Dimitrov <kanio.dimitrov@microsoft.com>
2017-09-21 19:36:20 +0900

    Adding messages support, moving to id centric logic and improving message to the user.

--- 6-voting-service/src/VotingStatusNode/index.js
+++ 6-voting-service/src/VotingStatusNode/index.js
@@ -1,16 +1,39 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    if(context.bindings.inputDocument && context.bindings.inputDocument.length == 1) {
+
+    if (context.bindings.inputDocument && context.bindings.inputDocument.length == 1)
+    {
+        var voting =  context.bindings.inputDocument[0];
+
+        var message = "Here we go, these are the current results - ";
+
+        for(var i=0; i < voting.options.length; i++){
+            var votes = voting.options[i].votes;
+            message += voting.options[i].text + " has " +  votes + (votes === 1 ? " vote" : " votes");
+            if(i < voting.options.length-1 ) { message += ", "}
+        }
+
+        var responseBody = {
+            "voting" : {
+                "votingname" : voting.votingname,
+                "isOpen" : voting.isOpen,
+                "question" : voting.question,
+                "options" : voting.options,
+                "id" : voting.id
+            },
+            "message" : message
+        };
+
         context.res = {
             status : 200,
-            body : context.bindings.inputDocument[0]
+            body : responseBody
         };
         context.done(null, context.res);
     }
     else {
         context.res = {
             status : 400,
-            body: "Record with this votingname can not be found. Please pass a votingname of an existing document in the request body"
+            body: { "message" : "Record with this id can not be found. Please pass an id of an existing document in the request body" }
         };
         context.done(null, context.res);
     }
